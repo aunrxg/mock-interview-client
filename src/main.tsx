3bd6321795b9e727adb7cd48e7573c02e588f3df
anuraginbytes <anuragpoddar9484@gmail.com>
2025-04-07 16:20:21 +0530

    add home, signin, signup

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { HomePage, SignInPage, SignUpPage } from './pages/index.ts'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='' element={<HomePage />} />
+      <Route path='login' element={<SignInPage />} />
+      <Route path='signup' element={<SignUpPage />} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
